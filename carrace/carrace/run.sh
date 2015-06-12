eaf43d8e7c3fd306936767d2c0014448a7fc34e7
曽和修平 <sowashuhei@gmail.com>
2015-06-13 01:56:56 +0900

    シェルスクリプトを書き換え（クラスファイルを一旦消してからコンパイルする）

--- carrace/carrace/run.sh
+++ carrace/carrace/run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 if [ "$1" -eq 2 ]; then
+	rm simplerace/KeyboardControllerOne.class
+	rm simplerace/MyController.class
+	rm simplerace/MyController2.class
+	javac simplerace/KeyboardControllerOne.java
 	javac simplerace/MyController.java
 	javac simplerace/MyController2.java
 	java simplerace.Play simplerace.MyController simplerace.MyController2
 else
+	rm simplerace/KeyboardControllerOne.class
+	rm simplerace/MyController.class
+	javac simplerace/KeyboardControllerOne.java
 	javac simplerace/MyController.java
 	java simplerace.Play simplerace.MyController 
 fi
